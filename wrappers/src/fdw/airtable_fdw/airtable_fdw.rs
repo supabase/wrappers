e3e45ca5bbd19f14f3d3ff2c2601bf695c829711
Ankur Goyal <ankur@impira.com>
2022-11-07 13:59:03 -0800

    Working on custom deserializer

--- wrappers/src/fdw/airtable_fdw/airtable_fdw.rs
+++ wrappers/src/fdw/airtable_fdw/airtable_fdw.rs
@@ -2,9 +2,14 @@ use pgx::log::PgSqlErrorCode;
 use reqwest::{self, header};
 use reqwest_middleware::{ClientBuilder, ClientWithMiddleware};
 use reqwest_retry::{policies::ExponentialBackoff, RetryTransientMiddleware};
-use serde::Deserialize;
 use serde_json::Value;
 use std::collections::HashMap;
+
+use serde::de::{self, MapAccess, Visitor};
+use serde::{Deserialize, Deserializer};
+use std::fmt;
+use std::marker::PhantomData;
+use std::str::FromStr;
 
 use supabase_wrappers::{
     create_async_runtime, report_error, require_option, Cell, ForeignDataWrapper, Limit, Qual, Row,
@@ -23,11 +28,71 @@ struct AirtableResponse {
     records: Vec<AirtableRecord>,
 }
 
+struct AirtableFields(HashMap<String, Value>);
+
+struct AirtableFieldsVisitor {
+    marker: PhantomData<fn() -> AirtableFields>,
+}
+
+impl AirtableFieldsVisitor {
+    fn new() -> Self {
+        AirtableFieldsVisitor {
+            marker: PhantomData,
+        }
+    }
+}
+
+// This is the trait that Deserializers are going to be driving. There
+// is one method for each type of data that our type knows how to
+// deserialize from. There are many other methods that are not
+// implemented here, for example deserializing from integers or strings.
+// By default those methods will return an error, which makes sense
+// because we cannot deserialize a AirtableFields from an integer or string.
+impl<'de> Visitor<'de> for AirtableFieldsVisitor {
+    // The type that our Visitor is going to produce.
+    type Value = AirtableFields;
+
+    // Format a message stating what data this Visitor expects to receive.
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("map")
+    }
+
+    // Deserialize AirtableFields from an abstract "map" provided by the
+    // Deserializer. The MapAccess input is a callback provided by
+    // the Deserializer to let us see each entry in the map.
+    fn visit_map<M>(self, mut access: M) -> Result<Self::Value, M::Error>
+    where
+        M: MapAccess<'de>,
+    {
+        let mut map = AirtableFields(HashMap::with_capacity(access.size_hint().unwrap_or(0)));
+
+        // While there are entries remaining in the input, add them
+        // into our map.
+        while let Some((key, value)) = access.next_entry::<String, Value>()? {
+            map.0.insert(key.to_lowercase(), value);
+        }
+
+        Ok(map)
+    }
+}
+
+// This is the trait that informs Serde how to deserialize AirtableFields.
+impl<'de> Deserialize<'de> for AirtableFields {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        // Instantiate our Visitor and ask the Deserializer to drive
+        // it over the input data, resulting in an instance of AirtableFields.
+        deserializer.deserialize_map(AirtableFieldsVisitor::new())
+    }
+}
+
 #[derive(Deserialize)]
 struct AirtableRecord {
     id: String,
-    fields: HashMap<String, Value>,
-    // XXX Incorporate the createdTime field?
+    fields: AirtableFields,
+    // TODO Incorporate the createdTime field? We'll need to deserialize as a timestamp
 }
 
 impl AirtableRecord {
@@ -47,15 +112,21 @@ impl AirtableRecord {
         }
     }
 
-    fn to_row(&self) -> Row {
+    fn to_row(&self, columns: &Vec<String>) -> Row {
         let mut row = Row::new();
-        row.push("id", Some(Cell::String(self.id.clone())));
-        row.push("name", Some(Cell::String("foo".to_string())));
-        /*
-        for (key, value) in self.fields.iter() {
-            row.push(key, AirtableRecord::value_to_cell(value));
+        for col in columns.iter() {
+            if col == "id" {
+                row.push("id", Some(Cell::String(self.id.clone())));
+            } else {
+                row.push(
+                    col,
+                    match self.fields.0.get(col) {
+                        Some(val) => AirtableRecord::value_to_cell(val),
+                        None => None,
+                    },
+                );
+            }
         }
-        */
         row
     }
 }
@@ -102,12 +173,12 @@ impl AirtableFdw {
     }
 
     // convert response body text to rows
-    fn resp_to_rows(&self, resp_body: &str) -> Vec<Row> {
+    fn resp_to_rows(&self, resp_body: &str, columns: &Vec<String>) -> Vec<Row> {
         let response: AirtableResponse = serde_json::from_str(resp_body).unwrap();
         let mut result = Vec::new();
 
         for record in response.records.iter() {
-            result.push(record.to_row());
+            result.push(record.to_row(columns));
         }
 
         result
@@ -123,12 +194,13 @@ macro_rules! report_fetch_error {
     };
 }
 
+// XXX Add support for INSERT
 impl ForeignDataWrapper for AirtableFdw {
     fn begin_scan(
         &mut self,
         // TODO: We should be able to propagate some of these through to airtable
         _quals: &Vec<Qual>,
-        _columns: &Vec<String>,
+        columns: &Vec<String>,
         _sorts: &Vec<Sort>,
         _limit: &Option<Limit>, // TODO: maxRecords
         options: &HashMap<String, String>,
@@ -150,7 +222,7 @@ impl ForeignDataWrapper for AirtableFdw {
                 Ok(resp) => match resp.error_for_status() {
                     Ok(resp) => {
                         let body = self.rt.block_on(resp.text()).unwrap();
-                        let result = self.resp_to_rows(&body);
+                        let result = self.resp_to_rows(&body, columns);
                         self.scan_result = Some(result);
                     }
                     Err(err) => report_fetch_error!(url, err),
