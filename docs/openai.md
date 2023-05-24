972644a5d8b9c43db86644512ddfd85979149bbb
Bo Lu <lv.patrick@gmail.com>
2023-05-24 15:32:32 +1000

    feat: add OpenAI fdw

---
+++ docs/openai.md
@@ -0,0 +1,207 @@
+[OpenAI](https://openai.com/) is an American artificial intelligence research laboratory consisting of the non-profit OpenAI Incorporated and its for-profit subsidiary corporation OpenAI Limited Partnership. OpenAI conducts AI research with the declared intention of promoting and developing a friendly AI.
+
+OpenAI FDW supports reading embedding data based on [text-embedding-ada-002 model](https://platform.openai.com/docs/guides/embeddings/second-generation-models), the embedding is in `string` format which can be saved to a `vector` column created by [pgvector](https://github.com/pgvector/pgvector).
+
+
+### Wrapper 
+To get started with the OpenAI wrapper, create a foreign data wrapper specifying `handler` and `validator` as below.
+
+```sql
+create extension if not exists wrappers;
+
+create foreign data wrapper openai_wrapper
+  handler openai_fdw_handler
+  validator openai_fdw_validator;
+```
+
+### Server 
+
+Next, we need to create a server for the FDW to hold options and credentials.
+
+#### Auth (Supabase)
+
+If you are using the Supabase platform, this is the recommended approach for securing your OpenAI API key.
+
+For example, to store OpenAI API key in Vault and retrieve the `key_id`,
+
+```sql
+-- save OpenAI API key in Vault and get its key id
+select pgsodium.create_key(name := 'openai');
+insert into vault.secrets (secret, key_id) values (
+  'sk-xxx',
+  (select id from pgsodium.valid_key where name = 'openai')
+) returning key_id;
+```
+
+Then create the foreign server,
+
+```sql
+do $$
+declare
+  key_id text;
+begin
+  select id into key_id from pgsodium.valid_key where name = 'openai' limit 1;
+
+  execute format(
+    E'create server openai_server \n'
+    '   foreign data wrapper openai_server \n'
+    '   options (api_key_id ''%s'');',
+    key_id
+  );
+end $$;
+```
+
+#### Auth (Non-Supabase)
+
+If the platform you are using does not support `pgsodium` and `Vault`, you can create a server by storing your OpenAI credentials directly.
+
+
+!!! important
+
+    Credentials stored using this method can be viewed as plain text by anyone with access to `pg_catalog.pg_foreign_server`
+
+
+```sql
+create server openai_server
+  foreign data wrapper openai_wrapper
+  options (
+    api_key 'sk-xxx'
+  );
+```
+
+
+### Tables
+
+OpenAI wrapper currently only supports reading [embedding](https://platform.openai.com/docs/guides/embeddings/what-are-embeddings) data through OpenAI API. The foreign table must contain `id` and `embedding` column.
+
+
+#### Foreign Table Options
+
+The full list of foreign table options are below:
+
+- `source` - Source table name in your database, required.
+
+   The source table must contain a primary key `id` column and an `input` column for embedding retrieving.
+
+   This can also be a subquery enclosed in parentheses, for example,
+
+   ```
+   source '(select * from my_table)'
+   ```
+
+   The subquery also needs to contain `id` and `input` columns.
+
+   Parameters are supported in the subquery. In this case, you need to define a column for each parameter and use query conditions to pass values to them. For example,
+
+   ```
+    create foreign table my_table (
+      id bigint,
+      embedding text,
+      _param1 text,
+      _param2 bigint
+    )
+      server openai_server
+      options (
+        source '(select id, input from my_source where column1=${_param1} and column2=${_param2})'
+      );
+
+    select * from my_table where _param1='aaa' and _param2=32;
+   ```
+
+
+#### Examples
+
+A simple example,
+
+```sql
+-- source table with column to store embedding
+-- Note: source table must have an 'id' and an 'input' column.
+create table my_table (
+  id bigint primary key,
+  input text,  -- input text to get embedding for
+  embedding vector(1536)  -- embedding for 'input' column based on text-embedding-ada-002 model
+);
+
+-- insert example input
+insert into my_table(id, input) values (1, 'hello world');
+
+-- foreign table to fetch embedding
+-- Note: foreign table must have an 'id' and an 'embedding' column,
+--       corresponding 'id' and 'input' columns in source table.
+create foreign table my_embedding (
+  id bigint,
+  embedding text  -- embedding in string format, e.g. '[0.0051006232, -0.03568436, ...]'
+)
+  server openai_server
+  options (
+    source 'my_table'  -- source table, required
+  );
+
+-- get embedding for each row of input text
+-- Note: this will call OpenAI API for each row, so the source table shouldn't have too many records.
+select * from my_embedding;
+
+-- save the embedding
+update my_table t
+set embedding = s.embedding::vector
+from my_embedding s
+where s.id = t.id;
+
+-- check if embedding has been saved
+select * from my_table;
+```
+
+This is another more complex example with query parameters, it incrementally updates embedding for specific input.
+
+```sql
+-- source table with an embedding column and an update timestamp
+create table my_table2 (
+  id bigint primary key,
+  author text,
+  content text,
+  embedding vector(1536),
+  ts timestamp
+);
+
+-- add some example data
+insert into my_table2(id, author, content, ts) values (1, 'foo', 'hello world', now());
+insert into my_table2(id, author, content, ts) values (2, 'bar', 'hello ai', now());
+
+-- foreign table to fetch embedding
+-- Note: foreign table must have an 'id' and an 'embedding' column.
+create foreign table my_embedding2 (
+  id bigint,
+  embedding text,
+  _param_author text, -- column for parameter ${author}
+  _param_ts text  -- column for parameter ${ts}
+)
+  server openai_server
+  options (
+    -- Note: the source sub-query must have an 'id' and an 'input' column, and the
+    -- corresponding parameters.
+    source '(
+      select id, content as input
+      from my_table2
+      where author = ''${_param_author}'' and ts >= ''${_param_ts}''
+    )'
+  );
+
+-- get embedding for each specified row of input text
+select id, embedding
+from my_embedding2
+where _param_author = 'foo'
+  and _param_ts = '2023-05-21';
+
+-- save the embedding
+update my_table2 t
+set
+  embedding = s.embedding::vector,
+  ts = now()
+from my_embedding2 s
+where s.id = t.id
+  and s._param_author = 'foo'
+  and s._param_ts = '2023-05-21';
+
+-- check if embedding has been saved
+select * from my_table2;
+```
---
+++ wrappers/src/fdw/openai_fdw/openai_fdw.rs
@@ -0,0 +1,281 @@
+use pgx::pg_sys;
+use pgx::prelude::PgSqlErrorCode;
+use pgx::spi::Spi;
+use regex::{Captures, Regex};
+use reqwest::{self, header};
+use reqwest_middleware::{ClientBuilder, ClientWithMiddleware};
+use reqwest_retry::{policies::ExponentialBackoff, RetryTransientMiddleware};
+use serde_json::Value as JsonValue;
+use std::collections::HashMap;
+use std::vec::IntoIter;
+
+use supabase_wrappers::prelude::*;
+
+fn create_client(api_key: &str) -> ClientWithMiddleware {
+    let mut headers = header::HeaderMap::new();
+    let value = format!("Bearer {}", api_key);
+    let mut auth_value = header::HeaderValue::from_str(&value).unwrap();
+    auth_value.set_sensitive(true);
+    headers.insert(header::AUTHORIZATION, auth_value);
+    let client = reqwest::Client::builder()
+        .default_headers(headers)
+        .build()
+        .unwrap();
+    let retry_policy = ExponentialBackoff::builder().build_with_max_retries(3);
+    ClientBuilder::new(client)
+        .with(RetryTransientMiddleware::new_with_policy(retry_policy))
+        .build()
+}
+
+macro_rules! report_error {
+    ($expr:expr) => {{
+        report_error(PgSqlErrorCode::ERRCODE_FDW_ERROR, $expr);
+        return None;
+    }};
+}
+
+#[wrappers_fdw(
+    version = "0.1.0",
+    author = "Supabase",
+    website = "https://github.com/supabase/wrappers/tree/main/wrappers/src/fdw/openai_fdw"
+)]
+pub(crate) struct OpenaiFdw {
+    rt: Runtime,
+    client: Option<ClientWithMiddleware>,
+    inputs: Option<IntoIter<HashMap<String, Cell>>>,
+    tgt_cols: Vec<Column>,
+    params: HashMap<String, Qual>,
+}
+
+impl OpenaiFdw {
+    const BASE_URL: &str = "https://api.openai.com/v1/embeddings";
+    const DEFAULT_MODEL: &str = "text-embedding-ada-002";
+
+    fn deparse(
+        &mut self,
+        source: &str,
+        quals: &[Qual],
+        columns: &[Column],
+        limit: &Option<Limit>,
+    ) -> String {
+        let source = if source.starts_with('(') {
+            let re = Regex::new(r"\$\{([^}]+)\}").unwrap();
+            re.replace_all(source, |caps: &Captures| {
+                let param = &caps[1];
+                match quals.iter().find(|&q| q.field == param) {
+                    Some(qual) => {
+                        self.params.insert(qual.field.clone(), qual.clone());
+                        match &qual.value {
+                            Value::Cell(cell) => match cell {
+                                Cell::String(s) => s.clone(),
+                                _ => {
+                                    report_error(
+                                        PgSqlErrorCode::ERRCODE_FDW_ERROR,
+                                        "query parameter must be a string",
+                                    );
+                                    String::default()
+                                }
+                            },
+                            Value::Array(_) => {
+                                report_error(
+                                    PgSqlErrorCode::ERRCODE_FDW_ERROR,
+                                    "invalid query parameter",
+                                );
+                                String::default()
+                            }
+                        }
+                    }
+                    None => {
+                        report_error(
+                            PgSqlErrorCode::ERRCODE_FDW_ERROR,
+                            &format!("unmatched query parameter: {}", param),
+                        );
+                        String::default()
+                    }
+                }
+            })
+            .into_owned()
+        } else {
+            source.to_string()
+        };
+        let source = format!("{} _wrappers_src", source);
+
+        let tgts = if columns.is_empty() {
+            "*".to_string()
+        } else {
+            let mut tgts = columns
+                .iter()
+                .filter(|c| !self.params.contains_key(&c.name) && c.name != "embedding")
+                .map(|c| c.name.clone())
+                .collect::<Vec<String>>()
+                .join(", ");
+
+            // add 'input' column if it is not included in target columns
+            if !columns.iter().any(|c| c.name == "input") {
+                tgts.push_str(", input");
+            }
+
+            tgts
+        };
+
+        let cond = quals
+            .iter()
+            .filter(|q| !self.params.contains_key(&q.field))
+            .map(|q| q.deparse())
+            .collect::<Vec<String>>();
+
+        let mut sql = if cond.is_empty() {
+            format!("select {} from {}", tgts, source)
+        } else {
+            format!(
+                "select {} from {} where {}",
+                tgts,
+                source,
+                cond.join(" and ")
+            )
+        };
+
+        if let Some(limit) = limit {
+            let real_limit = limit.offset + limit.count;
+            sql.push_str(&format!(" limit {}", real_limit));
+        }
+
+        sql
+    }
+}
+
+impl ForeignDataWrapper for OpenaiFdw {
+    fn new(options: &HashMap<String, String>) -> Self {
+        let client = match options.get("api_key") {
+            Some(api_key) => Some(create_client(api_key)),
+            None => require_option("api_key_id", options)
+                .and_then(|key_id| get_vault_secret(&key_id))
+                .map(|api_key| create_client(&api_key)),
+        };
+
+        OpenaiFdw {
+            rt: create_async_runtime(),
+            client,
+            inputs: None,
+            tgt_cols: Vec::new(),
+            params: HashMap::new(),
+        }
+    }
+
+    fn begin_scan(
+        &mut self,
+        quals: &[Qual],
+        columns: &[Column],
+        _sorts: &[Sort],
+        limit: &Option<Limit>,
+        options: &HashMap<String, String>,
+    ) {
+        let source = if let Some(source) = require_option("source", options) {
+            source
+        } else {
+            return;
+        };
+
+        self.tgt_cols = columns.to_vec();
+
+        // read source data
+        let sql = self.deparse(&source, quals, columns, limit);
+        self.inputs = Spi::connect(|client| {
+            let inputs = client
+                .select(&sql, None, None)
+                .map(|src_row| {
+                    let mut row = HashMap::new();
+                    row.insert("id".to_string(), src_row["id"].value::<Cell>().unwrap());
+                    row.insert(
+                        "input".to_string(),
+                        src_row["input"].value::<Cell>().unwrap(),
+                    );
+                    row
+                })
+                .collect::<Vec<_>>()
+                .into_iter();
+            Ok(Some(inputs))
+        });
+    }
+
+    fn iter_scan(&mut self, row: &mut Row) -> Option<()> {
+        if let Some(client) = &mut self.client {
+            if let Some(inputs) = &mut self.inputs {
+                if let Some(src_row) = inputs.next() {
+                    let mut body = HashMap::new();
+                    body.insert("model", Self::DEFAULT_MODEL.to_string());
+
+                    // find input and save it to request body
+                    if let Some(input) = src_row.get("input") {
+                        body.insert("input", input.to_string());
+                    } else {
+                        report_error!("no input column found");
+                    }
+
+                    // make api call to OpenAI
+                    let embedding = match self
+                        .rt
+                        .block_on(client.post(Self::BASE_URL).json(&body).send())
+                    {
+                        Ok(resp) => match self.rt.block_on(resp.json::<JsonValue>()) {
+                            Ok(result) => result
+                                .as_object()
+                                .map(|map| map.get("data").unwrap())
+                                .map(|arr| &arr[0])
+                                .map(|map| map.get("embedding").unwrap())
+                                .map(|arr| arr.as_array().unwrap())
+                                .map(|arr| {
+                                    format!(
+                                        "{:?}",
+                                        arr.iter()
+                                            .map(|em| em.as_f64().unwrap())
+                                            .collect::<Vec<f64>>()
+                                    )
+                                })
+                                .unwrap(),
+                            Err(err) => {
+                                report_error!(&format!("parse response to JSON failed: {}", err))
+                            }
+                        },
+                        Err(err) => report_error!(&format!("request OpanAI API failed: {}", err)),
+                    };
+
+                    // make output data row
+                    for col in &self.tgt_cols {
+                        match col.name.as_str() {
+                            "id" => row.push("id", src_row.get("id").cloned()),
+                            "embedding" => {
+                                row.push("embedding", Some(Cell::String(embedding.clone())))
+                            }
+                            _ => {
+                                // the rest columns should all be parameters
+                                let cell = self.params.get(&col.name).map(|q| match &q.value {
+                                    Value::Cell(cell) => cell.clone(),
+                                    _ => unreachable!(),
+                                });
+                                row.push(&col.name, cell)
+                            }
+                        }
+                    }
+
+                    return Some(());
+                }
+            }
+        }
+
+        None
+    }
+
+    fn end_scan(&mut self) {
+        self.inputs.take();
+        self.tgt_cols.clear();
+    }
+
+    fn validator(options: Vec<Option<String>>, catalog: Option<pg_sys::Oid>) {
+        if let Some(oid) = catalog {
+            if oid == FOREIGN_TABLE_RELATION_ID {
+                check_options_contain(&options, "source");
+            }
+        }
+    }
+}
