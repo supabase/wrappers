3fb0974347949d1f8a952af6d0f1b852d7c3e701
Bo Lu <lv.patrick@gmail.com>
2023-09-18 00:30:51 +1000

    chore(stripe_fdw): refactor error reporting

--- wrappers/src/fdw/stripe_fdw/mod.rs
+++ wrappers/src/fdw/stripe_fdw/mod.rs
@@ -1,3 +1,41 @@
 #![allow(clippy::module_inception)]
 mod stripe_fdw;
 mod tests;
+
+use pgrx::pg_sys::panic::ErrorReport;
+use pgrx::prelude::PgSqlErrorCode;
+use thiserror::Error;
+
+use supabase_wrappers::prelude::{CreateRuntimeError, OptionsError};
+
+#[derive(Error, Debug)]
+enum StripeFdwError {
+    #[error("column '{0}' data type is not supported")]
+    UnsupportedColumnType(String),
+
+    #[error("Stripe object '{0}' not implemented")]
+    ObjectNotImplemented(String),
+
+    #[error("{0}")]
+    OptionsError(#[from] OptionsError),
+
+    #[error("{0}")]
+    CreateRuntimeError(#[from] CreateRuntimeError),
+
+    #[error("parse url failed: {0}")]
+    UrlParseError(#[from] url::ParseError),
+
+    #[error("request failed: {0}")]
+    RequestError(#[from] reqwest_middleware::Error),
+
+    #[error("parse JSON response failed: {0}")]
+    JsonParseError(#[from] serde_json::Error),
+}
+
+impl From<StripeFdwError> for ErrorReport {
+    fn from(value: StripeFdwError) -> Self {
+        ErrorReport::new(PgSqlErrorCode::ERRCODE_FDW_ERROR, format!("{value}"), "")
+    }
+}
+
+type StripeFdwResult<T> = Result<T, StripeFdwError>;
