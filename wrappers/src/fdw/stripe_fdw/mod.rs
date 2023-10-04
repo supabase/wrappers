208d60a11748b78fb538bcbae08ee76bbb7b174a
Raminder Singh <romi_ssk@yahoo.co.in>
2023-10-04 10:57:29 +0530

    chore: remove a few unwrap calls

--- wrappers/src/fdw/stripe_fdw/mod.rs
+++ wrappers/src/fdw/stripe_fdw/mod.rs
@@ -2,6 +2,7 @@
 mod stripe_fdw;
 mod tests;
 
+use http::header::InvalidHeaderValue;
 use pgrx::pg_sys::panic::ErrorReport;
 use pgrx::prelude::PgSqlErrorCode;
 use thiserror::Error;
@@ -26,10 +27,22 @@ enum StripeFdwError {
     UrlParseError(#[from] url::ParseError),
 
     #[error("request failed: {0}")]
-    RequestError(#[from] reqwest_middleware::Error),
+    RequestError(#[from] reqwest::Error),
+
+    #[error("request middleware failed: {0}")]
+    RequestMiddlewareError(#[from] reqwest_middleware::Error),
 
     #[error("parse JSON response failed: {0}")]
     JsonParseError(#[from] serde_json::Error),
+
+    #[error("invalid api_key header: {0}")]
+    InvalidApiKeyHeader(#[from] InvalidHeaderValue),
+
+    #[error("invalid response")]
+    InvalidResponse,
+
+    #[error("invalid stats: {0}")]
+    InvalidStats(String),
 }
 
 impl From<StripeFdwError> for ErrorReport {
