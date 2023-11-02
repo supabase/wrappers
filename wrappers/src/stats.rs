abb7b70d3936c229c796ba2cf4c687e7bc36ce73
Raminder Singh <romi_ssk@yahoo.co.in>
2023-11-02 14:29:23 +0530

    chore: review comments feedback

--- wrappers/src/stats.rs
+++ wrappers/src/stats.rs
@@ -40,38 +40,37 @@ fn get_stats_table() -> String {
         .unwrap_or_else(|| panic!("cannot find fdw stats table '{}'", FDW_STATS_TABLE))
 }
 
+fn is_txn_read_only() -> bool {
+    Spi::get_one("show transaction_read_only") == Ok(Some("on"))
+}
+
 // increase stats value
 #[allow(dead_code)]
 pub(crate) fn inc_stats(fdw_name: &str, metric: Metric, inc: i64) {
-    if !is_txn_read_only() {
-        let sql = format!(
-            "insert into {} as s (fdw_name, {}) values($1, $2)
+    if is_txn_read_only() {
+        return;
+    }
+
+    let sql = format!(
+        "insert into {} as s (fdw_name, {}) values($1, $2)
          on conflict(fdw_name)
          do update set
             {} = coalesce(s.{}, 0) + excluded.{},
             updated_at = timezone('utc'::text, now())",
-            get_stats_table(),
-            metric,
-            metric,
-            metric,
-            metric
-        );
-        Spi::run_with_args(
-            &sql,
-            Some(vec![
-                (PgBuiltInOids::TEXTOID.oid(), fdw_name.into_datum()),
-                (PgBuiltInOids::INT8OID.oid(), inc.into_datum()),
-            ]),
-        )
-        .unwrap();
-    }
-}
-
-fn is_txn_read_only() -> bool {
-    let read_only_txn = Spi::get_one("show transaction_read_only")
-        .unwrap_or(Some("on"))
-        .unwrap_or("on");
-    read_only_txn == "on"
+        get_stats_table(),
+        metric,
+        metric,
+        metric,
+        metric
+    );
+    Spi::run_with_args(
+        &sql,
+        Some(vec![
+            (PgBuiltInOids::TEXTOID.oid(), fdw_name.into_datum()),
+            (PgBuiltInOids::INT8OID.oid(), inc.into_datum()),
+        ]),
+    )
+    .unwrap();
 }
 
 // get metadata
@@ -91,22 +90,24 @@ pub(crate) fn get_metadata(fdw_name: &str) -> Option<JsonB> {
 // set metadata
 #[allow(dead_code)]
 pub(crate) fn set_metadata(fdw_name: &str, metadata: Option<JsonB>) {
-    if !is_txn_read_only() {
-        let sql = format!(
-            "insert into {} as s (fdw_name, metadata) values($1, $2)
+    if is_txn_read_only() {
+        return;
+    }
+
+    let sql = format!(
+        "insert into {} as s (fdw_name, metadata) values($1, $2)
          on conflict(fdw_name)
          do update set
             metadata = $2,
             updated_at = timezone('utc'::text, now())",
-            get_stats_table()
-        );
-        Spi::run_with_args(
-            &sql,
-            Some(vec![
-                (PgBuiltInOids::TEXTOID.oid(), fdw_name.into_datum()),
-                (PgBuiltInOids::JSONBOID.oid(), metadata.into_datum()),
-            ]),
-        )
-        .unwrap();
-    }
+        get_stats_table()
+    );
+    Spi::run_with_args(
+        &sql,
+        Some(vec![
+            (PgBuiltInOids::TEXTOID.oid(), fdw_name.into_datum()),
+            (PgBuiltInOids::JSONBOID.oid(), metadata.into_datum()),
+        ]),
+    )
+    .unwrap();
 }
