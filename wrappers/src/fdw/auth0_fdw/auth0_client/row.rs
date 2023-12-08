a9ee01c870a7cae67e5fe7976fffa09b6fed5392
joel@joellee.org <joel@joellee.org>
2023-12-08 18:51:07 +0800

    feat: remove unused code

--- wrappers/src/fdw/auth0_fdw/auth0_client/row.rs
+++ wrappers/src/fdw/auth0_fdw/auth0_client/row.rs
@@ -1,5 +1,3 @@
-use crate::fdw::auth0_fdw::auth0_client::row::UserResponseError::ApiError;
-use crate::fdw::auth0_fdw::auth0_client::row::UserResponseError::MissingResult;
 use pgrx::JsonB;
 use serde::Deserialize;
 use serde::Serialize;
@@ -8,42 +6,12 @@ use std::collections::HashMap;
 use supabase_wrappers::prelude::Cell;
 use supabase_wrappers::prelude::Column;
 use supabase_wrappers::prelude::Row;
-use thiserror::Error;
 
 #[derive(Debug, Serialize, PartialEq)]
 pub(crate) struct UserRequest {
     limit: Option<u64>,
     offset: Option<u64>,
 }
-
-// pub(crate) struct UserRequestBuilder {
-//     request: UserRequest,
-// }
-
-// impl UserRequestBuilder {
-//     pub(crate) fn new() -> Self {
-//         Self {
-//             request: UserRequest {
-//                 limit: None,
-//                 offset: None,
-//             },
-//         }
-//     }
-
-//     pub(crate) fn limit(mut self, limit: Option<u64>) -> Self {
-//         self.request.limit = limit;
-//         self
-//     }
-
-//     pub(crate) fn offset(mut self, offset: Option<u64>) -> Self {
-//         self.request.offset = offset;
-//         self
-//     }
-
-//     pub(crate) fn build(self) -> UserRequest {
-//         self.request
-//     }
-// }
 
 #[derive(Debug, Deserialize, PartialEq)]
 pub struct ResultPayload {
@@ -58,23 +26,6 @@ pub(crate) struct Success {
     time: f64,
 }
 
-#[derive(Debug, Deserialize, PartialEq)]
-pub(crate) struct UserResponse {
-    error: Option<String>,
-    status: Option<String>,
-    result: Option<ResultPayload>,
-    time: Option<f64>,
-}
-
-#[derive(Error, Debug)]
-pub(crate) enum UserResponseError {
-    #[error("api response error: {0}")]
-    ApiError(String),
-
-    #[error("{0}")]
-    MissingResult(String),
-}
-
 #[derive(Debug)]
 pub struct Auth0Fields(HashMap<String, Value>);
 
@@ -84,22 +35,6 @@ pub struct Auth0User {
     pub email: String,
     pub email_verified: bool,
     pub identities: Option<serde_json::Value>,
-}
-
-impl UserResponse {
-    pub(crate) fn get_user_result(self) -> Result<ResultPayload, UserResponseError> {
-        if let Some(e) = self.error {
-            return Err(ApiError(e));
-        }
-
-        let Some(result) = self.result else {
-            return Err(MissingResult(
-                "`result` key missing in response".to_string(),
-            ));
-        };
-
-        Ok(result)
-    }
 }
 
 impl Auth0User {
