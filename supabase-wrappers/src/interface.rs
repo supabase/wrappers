6353c92739c80df718bd853a84ebb1c6ba57c943
Ming Ying <ming.ying.nyc@gmail.com>
2024-05-06 15:35:28 -0400

    time type

--- supabase-wrappers/src/interface.rs
+++ supabase-wrappers/src/interface.rs
@@ -3,7 +3,7 @@
 
 use crate::FdwRoutine;
 use pgrx::pg_sys::panic::ErrorReport;
-use pgrx::prelude::{Date, Timestamp, TimestampWithTimeZone};
+use pgrx::prelude::{Date, Time, Timestamp, TimestampWithTimeZone};
 use pgrx::{
     fcinfo,
     pg_sys::{self, BuiltinOid, Datum, Oid},
@@ -43,6 +43,7 @@ pub enum Cell {
     Numeric(AnyNumeric),
     String(String),
     Date(Date),
+    Time(Time),
     Timestamp(Timestamp),
     Timestamptz(TimestampWithTimeZone),
     Json(JsonB),
@@ -61,6 +62,7 @@ impl Clone for Cell {
             Cell::Numeric(v) => Cell::Numeric(v.clone()),
             Cell::String(v) => Cell::String(v.clone()),
             Cell::Date(v) => Cell::Date(*v),
+            Cell::Time(v) => Cell::Time(*v),
             Cell::Timestamp(v) => Cell::Timestamp(*v),
             Cell::Timestamptz(v) => Cell::Timestamptz(*v),
             Cell::Json(v) => Cell::Json(JsonB(v.0.clone())),
@@ -90,6 +92,13 @@ impl fmt::Display for Cell {
                     "'{}'",
                     dt_cstr.to_str().expect("date should be a valid string")
                 )
+            },
+            Cell::Time(v) => unsafe {
+                let tm =
+                    fcinfo::direct_function_call_as_datum(pg_sys::time_out, &[(*v).into_datum()])
+                        .unwrap();
+                let tm_cstr = CStr::from_ptr(tm.cast_mut_ptr());
+                write!(f, "'{}'", tm_cstr.to_str().unwrap())
             },
             Cell::Timestamp(v) => unsafe {
                 let ts = fcinfo::direct_function_call_as_datum(
@@ -148,6 +157,7 @@ impl IntoDatum for Cell {
             Cell::Numeric(v) => v.into_datum(),
             Cell::String(v) => v.into_datum(),
             Cell::Date(v) => v.into_datum(),
+            Cell::Time(v) => v.into_datum(),
             Cell::Timestamp(v) => v.into_datum(),
             Cell::Timestamptz(v) => v.into_datum(),
             Cell::Json(v) => v.into_datum(),
@@ -170,6 +180,7 @@ impl IntoDatum for Cell {
             || other == pg_sys::NUMERICOID
             || other == pg_sys::TEXTOID
             || other == pg_sys::DATEOID
+            || other == pg_sys::TIMEOID
             || other == pg_sys::TIMESTAMPOID
             || other == pg_sys::TIMESTAMPTZOID
             || other == pg_sys::JSONBOID
@@ -210,6 +221,9 @@ impl FromDatum for Cell {
             }
             PgOid::BuiltIn(PgBuiltInOids::DATEOID) => {
                 Date::from_datum(datum, is_null).map(Cell::Date)
+            }
+            PgOid::BuiltIn(PgBuiltInOids::TIMEOID) => {
+                Some(Cell::Time(Time::from_datum(datum, is_null).unwrap()))
             }
             PgOid::BuiltIn(PgBuiltInOids::TIMESTAMPOID) => {
                 Timestamp::from_datum(datum, is_null).map(Cell::Timestamp)
