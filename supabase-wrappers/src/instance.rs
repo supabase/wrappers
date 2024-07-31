d0603c97ce5eab7836067534a9d749e1cb535f4e
Rémi Lapeyre <remi.lapeyre@lenstra.fr>
2024-08-01 01:57:12 +0200

    Update instance.rs

--- supabase-wrappers/src/instance.rs
+++ supabase-wrappers/src/instance.rs
@@ -2,17 +2,6 @@ use crate::prelude::*;
 use pgrx::pg_sys::panic::ErrorReport;
 use pgrx::prelude::*;
 
-// create a fdw instance from a foreign table id
-pub(super) unsafe fn create_fdw_instance_from_table_id<
-    E: Into<ErrorReport>,
-    W: ForeignDataWrapper<E>,
->(
-    ftable_id: pg_sys::Oid,
-) -> W {
-    let ftable = pg_sys::GetForeignTable(ftable_id);
-    create_fdw_instance_from_server_id((*ftable).serverid)
-}
-
 // create a fdw instance from its id
 pub(super) unsafe fn create_fdw_instance_from_server_id<
     E: Into<ErrorReport>,
@@ -24,4 +13,15 @@ pub(super) unsafe fn create_fdw_instance_from_server_id<
     let fserver_opts = options_to_hashmap((*fserver).options).report_unwrap();
     let wrapper = W::new(&fserver_opts);
     wrapper.report_unwrap()
+}
+
+// create a fdw instance from a foreign table id
+pub(super) unsafe fn create_fdw_instance_from_table_id<
+    E: Into<ErrorReport>,
+    W: ForeignDataWrapper<E>,
+>(
+    ftable_id: pg_sys::Oid,
+) -> W {
+    let ftable = pg_sys::GetForeignTable(ftable_id);
+    create_fdw_instance_from_server_id((*ftable).serverid)
 }
