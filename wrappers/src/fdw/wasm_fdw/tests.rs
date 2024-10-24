8bed7e5735c1f2731c796ac97cb9007b4143cdac
Bo Lu <lv.patrick@gmail.com>
2024-10-24 17:53:57 +1100

    clean up comment

--- wrappers/src/fdw/wasm_fdw/tests.rs
+++ wrappers/src/fdw/wasm_fdw/tests.rs
@@ -14,7 +14,7 @@ mod tests {
             )
             .unwrap();
 
-            // Snowflake FDW test, test
+            // Snowflake FDW test
             c.update(
                 r#"CREATE SERVER snowflake_server
                      FOREIGN DATA WRAPPER wasm_wrapper
