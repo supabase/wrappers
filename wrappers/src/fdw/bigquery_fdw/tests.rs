d3d325ec230d097900b4047fe104b27ef8088ed1
Bo Lu <lv.patrick@gmail.com>
2025-05-20 01:00:35 +1000

    fix bigquery test failure

--- wrappers/src/fdw/bigquery_fdw/tests.rs
+++ wrappers/src/fdw/bigquery_fdw/tests.rs
@@ -6,6 +6,8 @@ mod tests {
 
     #[pg_test]
     fn bigquery_smoketest() {
+        rustls::crypto::aws_lc_rs::default_provider().install_default().unwrap();
+
         Spi::connect(|mut c| {
             c.update(
                 r#"CREATE FOREIGN DATA WRAPPER bigquery_wrapper
