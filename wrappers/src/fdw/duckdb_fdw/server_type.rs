5def4e09d9df4be16faf63bbfddeceedd10cbc69
rong fengliang <1141591465@qq.com>
2026-07-26 00:13:29 +0800

    Update server_type.rs

--- wrappers/src/fdw/duckdb_fdw/server_type.rs
+++ wrappers/src/fdw/duckdb_fdw/server_type.rs
@@ -212,7 +212,7 @@ impl ServerType {
                 let warehouse = require_option("warehouse", svr_opts)?;
                 let catalog_uri = require_option("catalog_uri", svr_opts)?;
                 let db_name = self.as_str();
-                let access_delegation_mode = require_option_or("accessDelegationMode", svr_opts,"vended_credentials");
+                let access_delegation_mode = require_option_or("access_delegation_mode", svr_opts,"vended_credentials");
                 format!(
                     "
                     attach '{warehouse}' as {db_name} (
